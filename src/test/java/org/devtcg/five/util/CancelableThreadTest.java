8e2b9a915a07a9eb81a92ec1443b4bc2b7a8b178
Josh Guilfoyle <jasta00@gmail.com>
2009-07-22 20:33:12 -0700

    Switched to maven and began unit test framework.

---
+++ src/test/java/org/devtcg/five/util/CancelableThreadTest.java
@@ -0,0 +1,75 @@
+package org.devtcg.five.util;
+
+import junit.framework.TestCase;
+
+public class CancelableThreadTest extends TestCase
+{
+	public void testThreadCancel() throws Exception
+	{
+		CancelableAndTestableThread thread = new CancelableAndTestableThread() {
+			private final Object mWaitUntilCanceled = new Object();
+
+			public void onRun() {
+				synchronized(mWaitUntilCanceled) {
+					while (hasCanceled() == false) {
+						try {
+							mWaitUntilCanceled.wait();
+						} catch (InterruptedException e) {}
+					}
+				}
+			}
+
+			@Override
+			public void requestCancel()
+			{
+				super.requestCancel();
+
+				synchronized(mWaitUntilCanceled) {
+					mWaitUntilCanceled.notify();
+				}
+			}
+		};
+
+		thread.start();
+		thread.waitForStartup();
+
+		/*
+		 * TODO: Add a requestCancelAndWait(long) method to wait up to a length
+		 * of time. This will help us report an error condition in the
+		 * CancelableThread class.
+		 */
+		thread.requestCancelAndWait();
+
+		/* If we made it here, the cancel worked. */
+	}
+
+	private abstract static class CancelableAndTestableThread extends CancelableThread
+	{
+		private boolean mStarted = false;
+		private final Object mStartLock = new Object();
+
+		public void run()
+		{
+			synchronized(mStartLock) {
+				mStarted = true;
+				mStartLock.notify();
+			}
+
+			onRun();
+		}
+
+		public abstract void onRun();
+
+		public void waitForStartup()
+		{
+			synchronized(mStartLock) {
+				while (mStarted == false)
+				{
+					try {
+						mStartLock.wait();
+					} catch (InterruptedException e) {}
+				}
+			}
+		}
+	}
+}
