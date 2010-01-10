1d4b4f90b94b229e9a401c7309b95d5947a985b2
Josh Guilfoyle <jasta@devtcg.org>
2010-01-09 19:56:59 -0800

    Temporarily increase maximum memory usage to 512M.
    This is necessary to work around an OutOfMemoryException thrown by the
    crawler thread.  Need to investigate later.

--- etc/five-server.sh
+++ etc/five-server.sh
@@ -18,4 +18,4 @@ libs="$libs:$base/libs/protobuf-2.0.3.jar"
 libs="$libs:$base/libs/commons-jxpath-1.1.jar"
 libs="$libs:$base/libs/sbbi-upnplib-1.0.4.jar"
 
-java -cp "$libs" org.devtcg.five.Main "$@"
+java -Xmx512M -cp "$libs" org.devtcg.five.Main "$@"
